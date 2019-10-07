557b30a5650aa2e48bd4de6dede83c521544ecf5
grayson-orr <graysono@op.ac.nz>
2019-10-08 01:29:40 +1300

    Remove empty tests

--- dropbox/test_dropbox_upload.py
+++ dropbox/test_dropbox_upload.py
@@ -17,12 +17,6 @@ class DropboxUploadTestCase(unittest.TestCase):
         self.assertEqual(
             self.files, ['final-results-test-1.pdf', 'final-results-test-2.pdf'])
 
-    def test_upload_single_file(self):
-        pass
-
-    def test_upload_multiple_files(self):
-        pass
-
     def tearDown(self):
         self.drpbx_upld = None
 
